240edd45ced629d4ca3940a96bece35cb7ccfc2d
MauricioMucci <mauricio_mucci@hotmail.com>
2023-07-08 16:19:49 -0300

    refactor: implementação dos métodos da interface, ajuste dos bits a serem comparados e adição das instruções no switch.
    Signed-off-by: MauricioMucci <mauricio_mucci@hotmail.com>

--- Maquina-Virtual/src/main/java/br/com/james/simulador/maquina/virtual/Mnemonico.java
+++ Maquina-Virtual/src/main/java/br/com/james/simulador/maquina/virtual/Mnemonico.java
@@ -9,6 +9,7 @@ import static br.com.james.simulador.maquina.virtual.RegistradorEnum.*;
  * @author yujisakuma
  */
 public enum Mnemonico implements Acao {
+    
     /**
      * 1. Quando adicionar um mnemonico adicionar o seu opcode em binário.
      * 2. Certificar que tenha 1 byte ou 2 bytes (depende da instrução). Além
@@ -20,6 +21,11 @@ public enum Mnemonico implements Acao {
             registradores.replace(IP, String.copyValueOf(instrucao.toCharArray(), 8, 16));
             return registradores;
         }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
+        }
     },
     JZ("01110100") {
         @Override
@@ -29,6 +35,11 @@ public enum Mnemonico implements Acao {
                 registradores.replace(IP, String.copyValueOf(instrucao.toCharArray(), 8, 16));
             }
             return registradores;
+        }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
         }
     },
     JNZ("01110101") {
@@ -40,6 +51,11 @@ public enum Mnemonico implements Acao {
             }
             return registradores;
         }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
+        }
     },
     JP("01111010") {
         @Override
@@ -50,114 +66,167 @@ public enum Mnemonico implements Acao {
             }
             return registradores;
         }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
+        }
     },
     AND_REG_REG("00100011"){
         @Override
         public Map<RegistradorEnum, String> acao(String instrucao, Map<RegistradorEnum, String> registradores) {
-            var ax = new StringBuilder(registradores.get(AX));
-            var dx = new StringBuilder(registradores.get(DX));
+            var ax = registradores.get(AX);
             var sr = new StringBuilder(registradores.get(SR)).reverse();
             
-            var operand = instrucao.substring(7, 15);
+            var operand = instrucao.substring(8, 16);
             var finalValue = new StringBuilder();
             
-            if (operand.equalsIgnoreCase(dx.toString())) {
-                for (int i = 0; i < ax.toString().toCharArray().length; i++) {
+            if (operand.equalsIgnoreCase(DX.getEndereco())) {
+                var dx = new StringBuilder(registradores.get(DX));
+                for (int i = 0; i < ax.toCharArray().length; i++) {
                     finalValue.append(ax.charAt(i) == '0' || dx.charAt(i) == '0' ? '0' : '1');
                 }
-            } else if (operand.equalsIgnoreCase(ax.toString())) {
+            } else if (operand.equalsIgnoreCase(AX.getEndereco())) {
                 finalValue.append(ax);
-            } else
-                throw new IllegalArgumentException();
+            } else {
+                throw new IllegalArgumentException(String.format("Operando: %s não existe!", operand));
+            }
             
-            fillRegisterSR(sr, finalValue);
+            fillRegisterSR(sr, finalValue, true);
             registradores.replace(SR, sr.reverse().toString());
             registradores.replace(AX, finalValue.toString());
             return registradores;
+        }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
         }
     },
     AND_REG_OP("00100101"){
         @Override
         public Map<RegistradorEnum, String> acao(String instrucao, Map<RegistradorEnum, String> registradores) {
-            return null;
+            var ax = registradores.get(AX);
+            var sr = new StringBuilder(registradores.get(SR)).reverse();
+            
+            var operand = instrucao.substring(8, 24);
+            var finalValue = new StringBuilder();
+            
+            if (ax.contentEquals("0".repeat(ax.toCharArray().length))){
+                finalValue.append(ax);
+            } else {
+                for (int i = 0; i < ax.toCharArray().length; i++) {
+                    finalValue.append(ax.charAt(i) == '0' || operand.charAt(i) == '0' ? '0' : '1');
+                }
+            }
+            
+            fillRegisterSR(sr, finalValue, true);
+            registradores.replace(SR, sr.reverse().toString());
+            registradores.replace(AX, finalValue.toString());
+            return registradores;
+        }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
         }
     },
     NOT_REG("11111000"){
         @Override
         public Map<RegistradorEnum, String> acao(String instrucao, Map<RegistradorEnum, String> registradores) {
-            var ax = new StringBuilder(registradores.get(AX));
+            var ax = registradores.get(AX);
             var sr = new StringBuilder(registradores.get(SR)).reverse();
             
-            var operand = instrucao.substring(7, 15);
+            var operand = instrucao.substring(8, 16);
             var finalValue = new StringBuilder();
             
-            if (operand.equalsIgnoreCase(ax.toString())) {
-                for (char bit : ax.toString().toCharArray()) {
+            if (operand.equalsIgnoreCase(AX.getEndereco())) {
+                for (char bit : ax.toCharArray()) {
                     finalValue.append(bit == '1' ? '0' : '1');
                 }
-            } else
-                throw new IllegalArgumentException();
+            } else {
+                throw new IllegalArgumentException(String.format("Operando: %s não existe!", operand));
+            }
             
-            fillRegisterSR(sr, finalValue);
+            fillRegisterSR(sr, finalValue, true);
             registradores.replace(SR, sr.reverse().toString());
             registradores.replace(AX, finalValue.toString());
             return registradores;
+        }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
         }
     },
     OR_REG_REG("00001011"){
         @Override
         public Map<RegistradorEnum, String> acao(String instrucao, Map<RegistradorEnum, String> registradores) {
-            var ax = new StringBuilder(registradores.get(AX));
-            var dx = new StringBuilder(registradores.get(DX));
+            var ax = registradores.get(AX);
             var sr = new StringBuilder(registradores.get(SR)).reverse();
             
-            var operand = instrucao.substring(7, 15);
+            var operand = instrucao.substring(8, 16);
             var finalValue = new StringBuilder();
             
-            if (operand.equalsIgnoreCase(dx.toString())) {
-                for (int i = 0; i < ax.toString().toCharArray().length; i++) {
+            if (operand.equalsIgnoreCase(DX.getEndereco())) {
+                var dx = new StringBuilder(registradores.get(DX));
+                for (int i = 0; i < ax.toCharArray().length; i++) {
                     finalValue.append(ax.charAt(i) == '1' || dx.charAt(i) == '1' ? '1' : '0');
                 }
-            } else if (operand.equalsIgnoreCase(ax.toString())) {
+            } else if (operand.equalsIgnoreCase(AX.getEndereco())) {
                 finalValue.append(ax);
-            } else
-                throw new IllegalArgumentException();
+            } else {
+                throw new IllegalArgumentException(String.format("Operando: %s não existe!", operand));
+            }
             
-            fillRegisterSR(sr, finalValue);
+            fillRegisterSR(sr, finalValue, true);
             registradores.replace(SR, sr.reverse().toString());
             registradores.replace(AX, finalValue.toString());
             return registradores;
+        }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
         }
     },
     OR_REG_OP("00001101"){
         @Override
         public Map<RegistradorEnum, String> acao(String instrucao, Map<RegistradorEnum, String> registradores) {
-            return null;
+            var ax = registradores.get(AX);
+            var sr = new StringBuilder(registradores.get(SR)).reverse();
+            
+            var operand = instrucao.substring(8, 24);
+            var finalValue = new StringBuilder();
+            
+            if (ax.contentEquals("1".repeat(ax.toCharArray().length))){
+                finalValue.append(ax);
+            } else {
+                for (int i = 0; i < ax.toCharArray().length; i++) {
+                    finalValue.append(ax.charAt(i) == '1' || operand.charAt(i) == '1' ? '1' : '0');
+                }
+            }
+            
+            fillRegisterSR(sr, finalValue, true);
+            registradores.replace(SR, sr.reverse().toString());
+            registradores.replace(AX, finalValue.toString());
+            return registradores;
+        }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
         }
     },
     XOR_REG_REG("00110011"){
         @Override
         public Map<RegistradorEnum, String> acao(String instrucao, Map<RegistradorEnum, String> registradores) {
-            var ax = new StringBuilder(registradores.get(AX));
-            var dx = new StringBuilder(registradores.get(DX));
-            var sr = new StringBuilder(registradores.get(SR)).reverse();
-            
-            var operand = instrucao.substring(7, 15);
-            var finalValue = new StringBuilder();
-            
-            if (operand.equalsIgnoreCase(dx.toString())) {
-                for (int i = 0; i < ax.toString().toCharArray().length; i++) {
-                    finalValue.append(ax.charAt(i) == dx.charAt(i) ? '0' : '1');
-                }
-            } else if (operand.equalsIgnoreCase(ax.toString())) {
-                finalValue.append("0".repeat(ax.toString().toCharArray().length));
-            } else
-                throw new IllegalArgumentException();
-            
-            fillRegisterSR(sr, finalValue);
-            registradores.replace(SR, sr.reverse().toString());
-            registradores.replace(AX, finalValue.toString());
-            return registradores;
+            return null;
+        }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
         }
     },
     XOR_REG_OP("00110101"){
@@ -165,36 +234,29 @@ public enum Mnemonico implements Acao {
         public Map<RegistradorEnum, String> acao(String instrucao, Map<RegistradorEnum, String> registradores) {
             return null;
         }
+        
+        @Override
+        public boolean isNumberOfBitsValid(String number) {
+            return false;
+        }
     };
     private final String valorBinario;
-
+    
     Mnemonico(String valorBinario) {
         this.valorBinario = valorBinario;
     }
     
-    public boolean isEven(String number) {
-        return number.split("1").length % 2 == 0;
-    }
-    public boolean isZero(String number) {
-        return !number.contains("1");
+    public String getValorBinario() {
+        return valorBinario;
     }
     
-    public void fillRegisterSR(StringBuilder sr, StringBuilder finalValue) {
-        sr.setCharAt(0, '0'); // CF (carry) = 0
-        sr.setCharAt(6, isEven(finalValue.toString()) ? '1' : '0'); // PF (parity) = isEven()
-        sr.setCharAt(8, isZero(finalValue.toString()) ? '1' : '0'); // ZF (zero) = isZero()
-        sr.setCharAt(9, finalValue.charAt(0)); // SF (sign) = most significant bit
-        sr.setCharAt(12, '0'); // OF (overflow) = 0
-    }
-    
+    // TODO: todos os opcodes devem estar aqui!!!
     /**
-     * 2.Não esquecer de colocar o upcode aqui tbm!
-     *
-     * @param bytes: bytens em String
+     * @param bits: bits em String
      * @return Mnemonico equivalente ao opcode
      */
-    public static Mnemonico getByBytes(String bytes) {
-        switch (bytes) {
+    public static Mnemonico getByBytes(String bits) {
+        switch (bits) {
             // DESVIO
             case "11101011" -> {
                 return JUMP;
@@ -234,9 +296,30 @@ public enum Mnemonico implements Acao {
             {
                 return XOR_REG_OP;
             }
+            case "11111000" ->
+            {
+                return NOT_REG;
+            }
             // -------------------------------------------------
             default ->
-                    throw new IllegalArgumentException("Não existe mnemônico que equivale a " + bytes);
+                    throw new IllegalArgumentException("Não existe mnemônico que equivale a " + bits);
         }
+    }
+    
+    private boolean isEven(String number) {
+        return (number.split("1").length - 1) % 2 == 0;
+    }
+    private boolean isZero(String number) {
+        return !number.contains("1");
+    }
+    
+    //TODO: quando a operação não for lógica indicar o que deve ser feito para o CF, IF e OF
+    public void fillRegisterSR(StringBuilder sr, StringBuilder finalValue, boolean isLogic) {
+        sr.setCharAt(0, isLogic ? '0' : '1'); // CF (carry) = 0
+        sr.setCharAt(6, isEven(finalValue.toString()) ? '1' : '0'); // PF (parity) = isEven()
+        // sr.setCharAt(7, isEven(finalValue.toString()) ? '1' : '0');
+        sr.setCharAt(8, isZero(finalValue.toString()) ? '1' : '0'); // ZF (zero) = isZero()
+        sr.setCharAt(9, finalValue.charAt(0)); // SF (sign) = most significant bit
+        sr.setCharAt(12, isLogic ? '0' : '1'); // OF (overflow) = 0
     }
 }
